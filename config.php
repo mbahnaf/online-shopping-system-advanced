5c4cec8bd5ef5f972b544bd987432c4930793af2
Nafiur Rashid <130646522+nafiursan@users.noreply.github.com>
2023-06-13 13:17:16 +0600

    Update config.php

--- config.php
+++ config.php
@@ -8,10 +8,10 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'supportReddy');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'ecommerece');
+define('DB_SERVER', 'ecommerce-db-instance.crkfp60kehfn.ap-northeast-2.rds.amazonaws.com');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'qwerty12');
+   define('DB_DATABASE', 'onlineshop');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
